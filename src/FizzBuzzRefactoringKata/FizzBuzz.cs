dd7a16f5003c7b2b2fe82d6638ba04615ebef8bc
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-07 16:44:11 +0100

    Add FizzBuzzRefactoring kata

---
+++ src/FizzBuzzRefactoringKata/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzzRefactoringKata
+{
+    public static class FizzBuzz
+    {
+        public static String Say(Int32 number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+    }
+}
---
+++ src/FizzBuzzRefactoringKata/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace FizzBuzzRefactoringKata
+{
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData(3, "Fizz")]
+        [InlineData(3 * 2, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(5 * 2, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(15 * 2, "FizzBuzz")]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(4, "4")]
+        [InlineData(8, "8")]
+        public void SingleNumber(Int32 input, String expected)
+        {
+            Assert.Equal(expected, FizzBuzz.Say(input));
+        }
+    }
+}
