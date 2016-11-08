cc91f59f18e70f2ae9bcf6e351cef91938b8e5fc
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-08 19:19:05 +0100

    Add more cases to CurrentCategoryPlace test

--- src/TriviaRefactoringKata/QuestionDeckTests.cs
+++ src/TriviaRefactoringKata/QuestionDeckTests.cs
@@ -10,6 +10,12 @@ namespace Trivia
         [InlineData(0, "Pop")]
         [InlineData(4, "Pop")]
         [InlineData(8, "Pop")]
+        [InlineData(1, "Science")]
+        [InlineData(5, "Science")]
+        [InlineData(9, "Science")]
+        [InlineData(2, "Sports")]
+        [InlineData(6, "Sports")]
+        [InlineData(10, "Sports")]
         public void CategoryForBoardPlace(Int32 place, String expected)
         {
             var deck = new QuestionDeck();
