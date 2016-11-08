d7e9c530deb92f8e9db07e831435a1734278706e
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-08 12:37:05 +0100

    Install test lib

---
+++ src/TriviaRefactoringKata/TryTestRunner.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Trivia
+{
+    public class TryTestRunner
+    {
+        [Fact]
+        public void TryMe()
+        {
+            Assert.Equal("b", "b");
+        }
+    }
+}
