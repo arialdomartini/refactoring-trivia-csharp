019def2b18705783c3cdacfefbcb1319a2f7e9bd
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-08 15:43:13 +0100

    Add characterization test via GoldenMaster

---
+++ src/TriviaRefactoringKata/GoldenMasterTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace Trivia
+{
+    public class GoldenMasterTests
+    {
+        const String ActualFile = "output1.txt";
+        const String GoldenMasterFile = @"..\..\golden-master.txt";
+
+        [Fact]
+        public void TriviaGame()
+        {
+            RunOneThousandGames();
+            var expected = ReadGoldenMaster();
+            var actual = ReadLastOutput();
+            Assert.Equal(expected, actual);
+        }
+
+        static String ReadLastOutput()
+        {
+            return File.ReadAllText(ActualFile);
+        }
+
+        static String ReadGoldenMaster()
+        {
+            return File.ReadAllText(GoldenMasterFile);
+        }
+
+        static void RunOneThousandGames()
+        {
+            using (var writer = File.CreateText(ActualFile))
+            {
+                Console.SetOut(writer);
+                for (var i = 0; i < 1000; i++)
+                {
+                    var seed = 34728 + 17 * i;
+                    GameRunner.Run(new Random(seed));
+                }
+            }
+        }
+    }
+}
