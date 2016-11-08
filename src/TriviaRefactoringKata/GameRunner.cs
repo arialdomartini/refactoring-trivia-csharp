f9f83ac765537ba04e1c12b35b146ea17f0d313f
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-08 13:19:11 +0100

    ATTENTION set fixed seed value in production

--- src/TriviaRefactoringKata/GameRunner.cs
+++ src/TriviaRefactoringKata/GameRunner.cs
@@ -20,7 +20,7 @@ namespace Trivia
             aGame.add("Pat");
             aGame.add("Sue");
 
-            Random rand = new Random();
+            Random rand = new Random(7);
 
             do
             {
