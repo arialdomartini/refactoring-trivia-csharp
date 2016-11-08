0218445130478d6f90f45e669c08566dcfdd3f36
Matteo Baglini <matteo.baglini@gmail.com>
2016-11-08 19:14:54 +0100

    Add one CurrentCategoryPlace test for Pop

---
+++ src/TriviaRefactoringKata/QuestionDeckTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Trivia
+{
+    public class QuestionDeckTests
+    {
+        [Fact]
+        public void CategoryForBoardPlace()
+        {
+            var deck = new QuestionDeck();
+
+            var category = deck.CurrentCategoryPlace(0);
+
+            Assert.Equal("Pop", category);
+        } 
+    }
+}
