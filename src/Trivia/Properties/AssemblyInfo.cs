b784945d76eb7f4e25bcb4748fc4c516188f3f3c
Arialdo Martini <arialdomartini@gmail.com>
2016-12-05 22:05:15 +0100

    It compiles on Mono 4.2.1

--- src/Trivia/Properties/AssemblyInfo.cs
+++ src/Trivia/Properties/AssemblyInfo.cs
@@ -5,22 +5,25 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Trivia")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Trivia")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2011")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cb7a7812-26eb-42fb-8a7d-a37933093ed1")]
+
+[assembly: Guid("181F96AB-4C36-48D5-A8C8-D434DCC820EF")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
