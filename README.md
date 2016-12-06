b3041b18c817f138b7117371f233328f7fcc6e9b
Arialdo Martini <arialdomartini@gmail.com>
2016-12-06 07:18:49 +0100

    A test that writes the program's output to a file

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Refactoring Trivia
 
 Exercise based on the [Trivia](https://github.com/caradojo/trivia) Legacy Code Retreat code.
+
+
+# Steps
+
+## Golden Master
+
+* Capture the program's output and check whether it's deterministic or not;
+* Repeat the execution a reasonable number of times, until the code coverage gives you enough confidence for your subsequent refactoring activities;
+* Collect the output in a file, and include it in the test project, using an error-prone manual operation (Poka-yoke); 
--- src/TriviaTest/DummyTest.cs
+++ src/TriviaTest/DummyTest.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Trivia;
 using Xunit;
 
 namespace TriviaTest
 {
-    public class DummyTest
+    public class DummyTest : IDisposable
     {
-        [Fact]
-        public void should_pass()
+        private TextWriter _originalOut;
+
+        public DummyTest()
         {
-            Assert.Equal("foo", "foo");
+            _originalOut = Console.Out;
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(_originalOut);
+        }
+
+        [Fact]
+        public void write_program_output_to_file()
+        {
+            using (var writer = File.CreateText("output.txt"))
+            {
+                Console.SetOut(writer);
+                foreach (var i in Enumerable.Range(0, 1000))
+                {
+                    GameRunner.Main(null);
+                }
+            }
         }
     }
 }
