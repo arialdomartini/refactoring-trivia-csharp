e5d6e03a3c40398fe0e18f141a69a848ddc67a00
Arialdo Martini <arialdomartini@gmail.com>
2016-12-06 10:18:03 +0100

    Testing wheter the output is deterministic

---
+++ src/TriviaTest/GoldenMasterTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Trivia;
+using Xunit;
+
+namespace TriviaTest
+{
+    public class GoldenMasterTest : IDisposable
+    {
+        private readonly TextWriter _originalOut;
+
+        public GoldenMasterTest()
+        {
+            _originalOut = Console.Out;
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(_originalOut);
+        }
+
+        [Fact]
+        public void checks_wheter_the_output_is_deterministic()
+        {
+            RunTheProgram(outputFile: "output1.txt", times: 1000);
+
+            RunTheProgram(outputFile: "output2.txt", times: 1000);
+
+            var actual1 = File.ReadAllText("output1.txt");
+            var actual2 = File.ReadAllText("output2.txt");
+            Assert.Equal(actual1, actual2);
+        }
+
+        private static void RunTheProgram(string outputFile, int times)
+        {
+            using (var writer = File.CreateText(outputFile))
+            {
+                Console.SetOut(writer);
+                foreach (var i in Enumerable.Range(0, times))
+                {
+                    GameRunner.Main(null);
+                }
+            }
+        }
+    }
+}
