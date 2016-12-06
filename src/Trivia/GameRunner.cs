50ac6de9796446d3833996254f61c96938ddcd7c
Arialdo Martini <arialdomartini@gmail.com>
2016-12-06 18:46:22 +0100

    Golden Master, using Peel

--- src/Trivia/GameRunner.cs
+++ src/Trivia/GameRunner.cs
@@ -14,17 +14,22 @@ namespace Trivia
 
         public static void Main(String[] args)
         {
+            var rand = new Random();
+
+            Run(rand);
+        }
+
+        public static void Run(Random rand)
+        {
             Game aGame = new Game();
 
             aGame.add("Chet");
             aGame.add("Pat");
             aGame.add("Sue");
 
-            Random rand = new Random();
 
             do
             {
-
                 aGame.roll(rand.Next(5) + 1);
 
                 if (rand.Next(9) == 7)
@@ -35,14 +40,8 @@ namespace Trivia
                 {
                     notAWinner = aGame.wasCorrectlyAnswered();
                 }
-
-
-
             } while (notAWinner);
-
         }
-
-
     }
 
 }
--- src/TriviaTest/GoldenMasterTest.cs
+++ src/TriviaTest/GoldenMasterTest.cs
@@ -9,6 +9,8 @@ namespace TriviaTest
     public class GoldenMasterTest : IDisposable
     {
         private readonly TextWriter _originalOut;
+        private const string ActualOutput = "output.txt";
+        private const string GoldenMaster = "../../golden-master.txt";
 
         public GoldenMasterTest()
         {
@@ -21,25 +23,37 @@ namespace TriviaTest
         }
 
         [Fact]
+        public void golden_master()
+        {
+            RunTheProgram(seed: 99, outputFile: ActualOutput, times: 1000);
+
+            var actual = File.ReadAllText(ActualOutput);
+            var goldenMaster = File.ReadAllText(GoldenMaster);
+
+            Assert.Equal(goldenMaster, actual);
+        }
+
+        [Fact]
         public void checks_wheter_the_output_is_deterministic()
         {
-            RunTheProgram(outputFile: "output1.txt", times: 1000);
+            RunTheProgram(seed: 99, outputFile: "output1.txt", times: 1000);
 
-            RunTheProgram(outputFile: "output2.txt", times: 1000);
+            RunTheProgram(seed: 99, outputFile: "output2.txt", times: 1000);
 
             var actual1 = File.ReadAllText("output1.txt");
             var actual2 = File.ReadAllText("output2.txt");
             Assert.Equal(actual1, actual2);
         }
 
-        private static void RunTheProgram(string outputFile, int times)
+        private static void RunTheProgram(int seed, string outputFile, int times)
         {
+            int times1;
             using (var writer = File.CreateText(outputFile))
             {
                 Console.SetOut(writer);
                 foreach (var i in Enumerable.Range(0, times))
                 {
-                    GameRunner.Main(null);
+                    GameRunner.Run(new Random(seed));
                 }
             }
         }
