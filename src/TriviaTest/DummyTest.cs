4194b14a6cab0e45c26e98e6c76ef9b6412ce261
Arialdo Martini <arialdomartini@gmail.com>
2016-12-06 05:59:12 +0100

    A empty, XUnit test project

---
+++ src/TriviaTest/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TriviaTest
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void should_pass()
+        {
+            Assert.Equal("foo", "foo");
+        }
+    }
+}
